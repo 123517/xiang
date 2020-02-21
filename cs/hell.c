54d40a1c54d689b6ee23d028b0a683aab9411a00
123 <3068232352@qq.com>
2020-02-21 22:01:42 +0800

    Update hell.c

--- cs/hell.c
+++ cs/hell.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-#define PRICE 23;
- void main()
- {
-     
-     int num,total;
-     num=18;
-     total=num*PRICE;
-     printf("total=%d\n",total);
- }
 
-void main()
+int main()
 {
-    char a,b;
-    b='B';
-    a+=32;
-    b+=32;
-    printf("%c,%c\n",a,b);
+    int max(int x,int y);
+    int a,b,c;
+    scanf("%d,%d",&a,&b);
+    c=max(a,b);
+    printf("max=%d\n",c);
+    return 0;
+}
+
+int max(int x,int y)
+{
+    int z;
+    if( x > y )z = x;
+    else z = y;
+    return(z);
 }
